b59001777c74009a8eaaf900723fb684a1b05362
shivendra gupta <you@example.com>
2023-01-05 11:44:02 +0530

    completed project

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::all();
+
+        return view('students.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Student::create([
+            'name' => request('name'),
+           'discription'=> request('discription'),
+        ]);
+
+        return redirect()->to(route('students.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $role = Student::findOrFail($id);
+
+        return view('students.show', compact('student'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $student = Student::findOrFail($id);
+
+        return view('students.edit', compact('student'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $student = Student::findOrFail($id);
+
+        $student->update([
+            'name' => request('name'),
+            'discription' => request('discription'),
+        ]);
+
+        return redirect(route('students.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $student = Student::findOrFail($id);
+
+        $student->delete();
+
+        return redirect(route('students.index'));
+    }
+}
---
+++ resources/views/students/create.blade.php
@@ -0,0 +1,24 @@
+<HTML>
+<body>
+
+    <form action="{{ route('students.store') }}" method="POST">
+        @csrf
+
+        <div>
+            <label for="">Name</label>
+
+            <input type="text" name="name">
+        </div>   
+       
+        <div>
+            <label for="">Description</label>
+
+            <input type="text" name="discription">
+        </div>
+
+        <div>
+            <input type="submit">
+        </div>
+    </form>
+</body>
+</html>
