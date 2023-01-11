8c904e083580f082c979e772069c740b19a21385
Abhishek Kathuria <abhishekkathuria.oc522@webkul.in>
2023-01-11 13:33:48 +0530

    Vue JS Some Work Done

--- packages/Webkul/Project/src/Resources/views/students/index.blade.php
+++ packages/Webkul/Project/src/Resources/views/students/index.blade.php
@@ -1,8 +1,8 @@
-
 <!DOCTYPE html>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <html lang="en">
-    <head><h3>CRUD OPERATION</h3>
+    <head>
+        <h3>CRUD OPERATION</h3>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <style>
             table{
                 border: 2px solid;
@@ -38,43 +38,86 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
     </head>
     <body>
+        <div id="main">
+            <table border=5px>
+                <thead>
+                    <td> {{ __('project::app.id') }}</td>
+                    <td> {{ __('project::app.name') }}</td>
+                    <td> {{ __('project::app.description') }}</td>
+                    <td> {{ __('project::app.edit') }}</td>
+                    <td> {{ __('project::app.delete') }}</td>
+                </thead>
         
-    <table border=5px>
-        <thead>
-            <td> {{ __('project::app.id') }}</td>
-            <td> {{ __('project::app.name') }}</td>
-            <td> {{ __('project::app.description') }}</td>
-            <td> {{ __('project::app.edit') }}</td>
-            <td> {{ __('project::app.delete') }}</td>
-        </thead>
-
-        <tbody>
-            @foreach ($students as $student)
-                <tr>
-                    <td>{{ $student->id }}</td>
-                    <td>{{ $student->name }}</td>
-                    <td>{{ $student->discription }}</td>
-                    <td><a href="{{route('students.edit',  $student->id)}}"><i class="fa fa-edit"></i></a> </td>
-                    <td>
-                        <form id="studentDeleteForm{{$student->id}}" action="{{ route('students.destroy', $student->id) }}" method="POST">
-
-                            @csrf
+                <tbody>
+                    @foreach ($students as $student)
+                        <tr>
+                            <td>{{ $student->id }}</td>
+                            <td>{{ $student->name }}</td>
+                            <td>{{ $student->discription }}</td>
+                            <td><a href="{{route('students.edit',  $student->id)}}"><i class="fa fa-edit"></i></a> </td>
+                            <td>
+                                <form id="studentDeleteForm{{$student->id}}" action="{{ route('students.destroy', $student->id) }}" method="POST">
+        
+                                    @csrf
+                                    
+                                    @method('DELETE')
+                                </form>
                             
-                            @method('DELETE')
-                        </form>
-                    
-                    <div>
-                        <a href="#" onclick="document.getElementById('studentDeleteForm{{$student->id}}').submit();"><i class="fa fa-trash"></i></a>
-                    </div>
+                            <div>
+                                <a href="#" onclick="document.getElementById('studentDeleteForm{{$student->id}}').submit();"><i class="fa fa-trash"></i></a>
+                            </div>
+        
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            
+            <a href="students/create">
+                <button>{{ __('project::app.add') }}</button>
+            </a>
+            
+            <table-row-component username="Devansh" class-name="9" roll-number="12"></table-row-component>
+        </div>  
+        
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.js"></script>
 
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-        <a href="students/create">
-            <button>{{ __('project::app.add') }}</button>
-        </a>
-      
+        <script type="text/x-template" id="table-row-template">
+            <div>
+                <div v-text="username" @click="alertUsername"></div>
+
+                <div v-text="className" @click="alertClassName"></div>
+                
+                <div v-text="rollNumber" @click="alertRollNumber"></div>
+            </div>
+        </script>
+        
+        <script>
+            Vue.component('table-row-component', {
+                template: "#table-row-template",
+
+                props: ['username', 'className', 'rollNumber'],
+
+                methods: {
+                    alertUsername() {
+                        alert(this.username);
+                    },
+
+                    alertClassName() {
+                        alert(this.className);
+                    },
+
+                    alertRollNumber() {
+                        alert(this.rollNumber);
+                    },
+                },
+            });
+        </script>
+
+        <script>
+            new Vue({
+                el: "#main",
+            });
+        </script>
     </body>
 </html> 
  
